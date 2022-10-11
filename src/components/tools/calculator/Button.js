268a39d8a3ecd20c20af866eafaea0af38e1b2e5
VojinPopovic <vojinpopovic3@gmail.com>
2022-10-11 18:37:39 +0200

    finished calculator logic

--- src/components/tools/calculator/Button.js
+++ src/components/tools/calculator/Button.js
@@ -6,11 +6,13 @@ function Button(props) {
 
   function calculate() {
     let symbols = ["%", "/", "*", "-", "+", ".", "="];
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
     if (display === "" && symbols.includes(props.value)) {
       alert("Can't use operator at the start of the expression");
     } else if (props.value === "c") {
       setDisplay("");
-    } else if (props.value === "=") {
+    } else if (props.value === "=" && numbers.includes(display.slice(-1))) {
       let evaluation = display;
       evaluation.replace(/[^-()\d/*+.]/g, "");
       setDisplay(eval(evaluation));
